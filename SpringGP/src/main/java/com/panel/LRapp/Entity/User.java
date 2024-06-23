b95c097bc3b9b1d31558f3def8bfc135a3c82cb0
SaraMohmed <saradawaly003@gmail.com>
2024-06-24 01:56:27 +0300

    update files

--- SpringGP/src/main/java/com/panel/LRapp/Entity/User.java
+++ SpringGP/src/main/java/com/panel/LRapp/Entity/User.java
@@ -125,7 +125,7 @@ public class User implements UserDetails {
     @OneToMany(fetch = FetchType.LAZY,mappedBy = "user",cascade = CascadeType.ALL)
     private List<UserChallengePublic> userChallengePublics;
 
-    @OneToMany(fetch = FetchType.LAZY,mappedBy = "user",cascade = CascadeType.ALL)
+    @OneToMany(fetch = FetchType.LAZY,mappedBy = "user",cascade = CascadeType.ALL, orphanRemoval = true)
     private Set<Posts> posts = new HashSet<>();
 //    private List<Posts> posts;
 
