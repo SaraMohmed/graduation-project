883a33d5edc6939bfc16d8631d3c7cdda9d50085
SaraMohmed <saradawaly003@gmail.com>
2024-07-04 12:52:52 +0300

    update files

--- SpringGP/src/main/java/com/panel/LRapp/Service/AdminCService.java
+++ SpringGP/src/main/java/com/panel/LRapp/Service/AdminCService.java
@@ -1,9 +1,11 @@
 package com.panel.LRapp.Service;
 
+import com.panel.LRapp.Dto.AdminDTO;
 import com.panel.LRapp.Dto.feelingsDTO;
 import com.panel.LRapp.Entity.AdminChallenge;
 import com.panel.LRapp.Entity.feelings;
 import com.panel.LRapp.response.AdminCResponse;
+import com.panel.LRapp.response.CRAdminCU;
 import com.panel.LRapp.response.feelingsResponse;
 
 import java.util.List;
@@ -11,7 +13,9 @@ import java.util.List;
 public interface AdminCService {
     AdminCResponse save(AdminChallenge adminChallenge);
     void delete(int id);
+
     AdminCResponse update(AdminChallenge adminChallenge);
+//    CRAdminCU updateCR(String token, AdminDTO adminDTO);
     AdminCResponse findById(int id);
 
     List<AdminChallenge> findByName(String name);
--- SpringGP/src/main/java/com/panel/LRapp/Service/AdminCServiceImpl.java
+++ SpringGP/src/main/java/com/panel/LRapp/Service/AdminCServiceImpl.java
@@ -1,12 +1,18 @@
 package com.panel.LRapp.Service;
 
 
+import com.panel.LRapp.Dto.AdminDTO;
 import com.panel.LRapp.Entity.AdminCDays;
 import com.panel.LRapp.Entity.AdminChallenge;
 
+import com.panel.LRapp.Entity.Token;
+import com.panel.LRapp.Entity.User;
 import com.panel.LRapp.Repo.AdminCRepo;
+import com.panel.LRapp.Repo.TokenRepository;
+import com.panel.LRapp.Repo.UserRepo;
 import com.panel.LRapp.response.AdminCResponse;
 
+import com.panel.LRapp.response.CRAdminCU;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
@@ -19,6 +25,12 @@ import java.util.Optional;
 public class AdminCServiceImpl implements AdminCService{
     @Autowired
     private AdminCRepo adminCRepo;
+
+    @Autowired
+    private TokenRepository tokenRepository;
+
+    @Autowired
+    private UserRepo userRepo;
 
     @Override
     public AdminCResponse save(AdminChallenge adminChallenge) {
@@ -53,6 +65,24 @@ public class AdminCServiceImpl implements AdminCService{
          adminCRepo.deleteById(id);
     }
 
+
+//    @Override
+//    public AdminCResponse update(AdminChallenge adminChallenge) {
+//        Optional<AdminChallenge> adminChallenge1=adminCRepo.findById(adminChallenge.getId());
+//        if(adminChallenge1.isEmpty()){
+//            return new AdminCResponse("لم يتم العثور على هذا التحدي :(",null);
+//        }else{
+//            adminChallenge1.get().setName(adminChallenge.getName());
+//            adminChallenge1.get().setDescription(adminChallenge.getDescription());
+//            adminChallenge1.get().setIcon1(adminChallenge.getIcon1());
+//            adminChallenge1.get().setIcon2(adminChallenge.getIcon2());
+//            adminChallenge1.get().setDone(adminChallenge.isDone());
+//            adminChallenge1.get().setAdminCDays(adminChallenge.getAdminCDays());
+//
+//            return new AdminCResponse("تم التعديل بنجاح :)",adminCRepo.save(adminChallenge1.get()));
+//        }
+//    }
+
     @Override
     public AdminCResponse update(AdminChallenge adminChallenge) {
         Optional<AdminChallenge> adminChallenge1=adminCRepo.findById(adminChallenge.getId());
@@ -63,12 +93,39 @@ public class AdminCServiceImpl implements AdminCService{
             adminChallenge1.get().setDescription(adminChallenge.getDescription());
             adminChallenge1.get().setIcon1(adminChallenge.getIcon1());
             adminChallenge1.get().setIcon2(adminChallenge.getIcon2());
-            adminChallenge1.get().setDone(adminChallenge.isDone());
-            adminChallenge1.get().setAdminCDays(adminChallenge.getAdminCDays());
+
+
+            List<AdminCDays> adminCDays=adminChallenge1.get().getAdminCDays();
+            List<AdminCDays> adminCDays1=adminChallenge.getAdminCDays();
+            for (int i = 0; i < adminCDays1.size(); i++) {
+                adminChallenge1.get().getAdminCDays().get(i).setName(adminCDays1.get(i).getName());
+                adminChallenge1.get().getAdminCDays().get(i).setContent(adminCDays1.get(i).getContent());
+
+            }
+            adminChallenge1.get().setAdminCDays(adminCDays);
 
             return new AdminCResponse("تم التعديل بنجاح :)",adminCRepo.save(adminChallenge1.get()));
         }
     }
+
+//    @Override
+//    public CRAdminCU updateCR(String token, AdminDTO adminDTO) {
+//        Token to= tokenRepository.findByToken(token.substring(7));
+//        User user=userRepo.findByEmail(to.getUser().getEmail());
+//        Optional<AdminChallenge> adminChallenge=adminCRepo.findById(adminDTO.getId());
+//        List<AdminChallenge> adminChallenges=user.getAdminChallengeSet();
+//        for(int i=0;i<adminChallenges.size();i++){
+//            if(adminChallenges.get(i)==adminChallenge.get()){
+//                user.getAdminChallengeSet().get(i).setDone(adminDTO.isDone1());
+//                for (int j = 0; j < adminChallenge.get().getAdminCDays().size(); j++) {
+//                    user.getAdminChallengeSet().get(i).getAdminCDays().get(j).setDone(adminDTO.getDays().get(i).isDone2());
+//                }
+//            }
+//        }
+//        userRepo.save(user);
+//
+//        return new CRAdminCU("تم التعديل بنجاح :)");
+//    }
 
     @Override
     public AdminCResponse findById(int id) {
